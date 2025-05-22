73731cfc6078eff678ed2eed8a4d4dcfe496ea41
Khanh2720 <b11200037@std.must.edu.tw>
2025-05-22 18:00:49 +0800

    Added EmailAddressValidation class

--- Resources/EmailAddressValidation.cs
+++ Resources/EmailAddressValidation.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace MovieTicketBookingManagementSystem.Resources
 {
-    internal class EmailAddressValidation
+    public static class EmailAddressValidation
     {
+         public static bool IsValid(string email)
+        {
+            string regex = @"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$";
+            return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
+        }
     }
 }
